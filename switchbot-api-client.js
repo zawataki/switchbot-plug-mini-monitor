0f7f500fd24b10dd65c921bb203911b27090e6a2
Yuki Takizawa <y.takizawa64@gmail.com>
2024-03-14 13:12:22 +0900

    Bugfix: SwitchBot API returns 401 error after sometime
    SwitchBot API returned 401 (Unauthorized) errors about 10 minutes
    after starting the script.
    Because the timestamp in API request headers was not updated
    when sending API requests.

--- switchbot-api-client.js
+++ switchbot-api-client.js
@@ -4,25 +4,29 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const currentDateTime = Date.now();
-const nonce = crypto.randomUUID();
 const API_URL = 'https://api.switch-bot.com/v1.1';
-const API_OPTIONS = {
-  headers: {
-    "Authorization": process.env.SWITCHBOT_API_TOKEN,
-    "sign": crypto.createHmac('sha256', process.env.SWITCHBOT_API_SECRET)
-      .update(Buffer.from(process.env.SWITCHBOT_API_TOKEN + currentDateTime + nonce, 'utf-8'))
-      .digest()
-      .toString("base64"),
-    "nonce": nonce,
-    "t": currentDateTime,
-    'Content-Type': 'application/json; charset=utf8',
-  },
-};
+
+function generateApiOptions() {
+  const currentDateTime = Date.now();
+  const nonce = crypto.randomUUID();
+  return {
+    headers: {
+      "Authorization": process.env.SWITCHBOT_API_TOKEN,
+      "sign": crypto.createHmac('sha256', process.env.SWITCHBOT_API_SECRET)
+        .update(Buffer.from(process.env.SWITCHBOT_API_TOKEN + currentDateTime + nonce, 'utf-8'))
+        .digest()
+        .toString("base64"),
+      "nonce": nonce,
+      "t": currentDateTime,
+      'Content-Type': 'application/json; charset=utf8',
+    },
+  };
+}
 
 export async function getDeviceList() {
   try {
-    const response = await got.get(`${API_URL}/devices`, API_OPTIONS).json();
+    const response = await got.get(`${API_URL}/devices`, generateApiOptions())
+      .json();
     return response.body;
   } catch (error) {
     throw new Error("Failed to get device list", {cause: error});
@@ -31,7 +35,8 @@ export async function getDeviceList() {
 
 export async function getDeviceStatus(deviceId) {
   try {
-    const response = await got.get(`${API_URL}/devices/${deviceId}/status`, API_OPTIONS).json();
+    const response = await got.get(`${API_URL}/devices/${deviceId}/status`, generateApiOptions())
+      .json();
     return response.body;
   } catch (error) {
     throw new Error("Failed to get device status", {cause: error});
@@ -40,7 +45,8 @@ export async function getDeviceStatus(deviceId) {
 
 export async function getScenes() {
   try {
-    const response = await got.get(`${API_URL}/scenes`, API_OPTIONS).json();
+    const response = await got.get(`${API_URL}/scenes`, generateApiOptions())
+      .json();
     return response.body;
   } catch (error) {
     throw new Error("Failed to get scenes", {cause: error});
@@ -49,7 +55,8 @@ export async function getScenes() {
 
 export async function executeManualScene(sceneId) {
   try {
-    const response = await got.post(`${API_URL}/scenes/${sceneId}/execute`, API_OPTIONS).json();
+    const response = await got.post(`${API_URL}/scenes/${sceneId}/execute`, generateApiOptions())
+      .json();
     return response.body;
   } catch (error) {
     throw new Error("Failed to execute manual scene", {cause: error});
